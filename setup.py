c2dbc96f36e973d90d195e08869c66d654f4c0e8
kgullikson88 <kevin.gullikson@gmail.com>
2014-01-09 01:35:49 -0600

    Changed the file structure: The python (and cython) source are in the src directory. I also added tar files with the lblrtm stuff, but am not adding it to the git repo!

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ import numpy
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("FittingUtilities", ["FittingUtilities.pyx"], include_dirs=[numpy.get_include()], extra_compile_args=["-O3", "-funroll-loops"]),]
+    ext_modules = [Extension("FittingUtilities", ["src/FittingUtilities.pyx"], include_dirs=[numpy.get_include()], extra_compile_args=["-O3", "-funroll-loops"]),],
+    #package_dir = {'': 'src' }
     )
