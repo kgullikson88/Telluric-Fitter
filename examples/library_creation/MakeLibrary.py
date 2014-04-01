3f915b061446a0677a4dfca2f796bd97332f1049
kgullikson <kevin.gullikson@gmail.com>
2014-04-01 11:21:01 -0500

    Updated the library_creation example to work correctly. How did I not notice that before!?

--- examples/library_creation/MakeLibrary.py
+++ examples/library_creation/MakeLibrary.py
@@ -2,14 +2,17 @@ import MakeModel
 
 
 humidity = range(10, 90, 10)   #Let humidity go from 10% to 90%, in steps of 10%
-co2 = range(350, 400, 5)  #Let the CO2 surface abundance range from 350-400 ppmv, in steps of 5
-wavestart = 1500.0
-waveend = 1550.0
+o2 = [1.5e5, 2.1e5, 2.5e5]  #Let the O2 surface abundance range vary too
+
+# Set the start and end wavelength, in nm!
+wavestart = 500
+waveend = 900
 
 #Make an instance of the Modeler class
 modeler = MakeModel.Modeler()
-for h20 in humidity:
-  for co2val in co2:
-    modeler.MakeModel(h2o=h2o, co2=co2val, save=True, libfile="Library_Files.dat", lowfreq=1e7/waveend, highfreq=1e7/wavestart)
+for h2o in humidity:
+  for o2val in o2:
+    modeler.MakeModel(humidity=h2o, o2=o2val, save=True, libfile="Library_Files.dat", lowfreq=1e7/waveend, highfreq=1e7/wavestart)
 
-#The filenames of your telluric library are given in the file 'Library_Files.dat' in the current directory.
+print "The filenames of your telluric library are given in the file 'Library_Files.dat' in the current directory."
+print "You can move them all here with the script move_library_here.sh"
