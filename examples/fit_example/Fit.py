c7d78ccd63e6a2b1136eb9708fba3233ad94ea80
kgullikson88 <kevin.gullikson@gmail.com>
2014-04-04 11:27:11 -0500

    Changed license from GPL to MIT. I have not officially distributed it yet so I think it is fine...

--- examples/fit_example/Fit.py
+++ examples/fit_example/Fit.py
@@ -1,3 +1,21 @@
+"""
+
+    This file is part of the TelFit program.
+
+    TelFit is free software: you can redistribute it and/or modify
+    it under the terms of the MIT license.
+
+    TelFit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    You should have received a copy of the MIT license
+    along with TelFit.  If not, see <http://opensource.org/licenses/MIT>.
+
+
+"""
+
+
 import numpy
 import matplotlib.pyplot as plt
 import TelluricFitter
--- src/MakeModel.py
+++ src/MakeModel.py
@@ -11,17 +11,14 @@
     This file is part of the TelFit program.
 
     TelFit is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    it under the terms of the MIT license.
 
     TelFit is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-    You should have received a copy of the GNU General Public License
-    along with TelFit.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the MIT license
+    along with TelFit.  If not, see <http://opensource.org/licenses/MIT>.
 
 
 """
