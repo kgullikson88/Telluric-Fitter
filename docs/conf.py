cb59596e110bb1380c1c1652eac1d5eae61820d3
kgullikson88 <kevin.gullikson@gmail.com>
2015-06-07 14:14:04 -0500

    added numpy/scipy/matplotlib to mock

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['FittingUtilities']
+MOCK_MODULES = ['FittingUtilities', 'numpy', 'scipy', 'matplotlib']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
