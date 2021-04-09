e09048c59042a8068fcc7189af167e11846a6826
sytang-astro <sytang@lowell.edu>
2021-04-09 15:58:54 -0700

    passing air_wave into GenerateModel fun

--- src/TelluricFitter.py
+++ src/TelluricFitter.py
@@ -609,7 +609,7 @@ class TelluricFitter:
 
 
 
-    def GenerateModel(self, pars, nofit=False, separate_source=False, return_resolution=False, broaden=False, model=None):
+    def GenerateModel(self, pars, nofit=False, separate_source=False, return_resolution=False, broaden=False, model=None, air_wave=True):
         """
         This function does the actual work of generating a model with the given parameters,
         fitting the continuum, making sure the model and data are well aligned in
@@ -626,7 +626,8 @@ class TelluricFitter:
                         Ignored if nofit=False
         :param model: A DataStructures.xypoint instance containing an un-broadened telluric model.
                       If given, it uses this instead of making one.
-
+        :param air_wave:  Are the wavelengths in air wavelengths? Default is True.
+        
         :return:  The best-fit telluric model, as a DataStructures.xypoint instance where the x-axis is
                  sampled the same as the data (so you should be able to directly divide the two). If
                  separate_source = True, this method also returns the estimate for the source spectrum *before*
@@ -635,6 +636,7 @@ class TelluricFitter:
                  source_spec, model_spec, resolution
         """
         data = self.data
+        self.air_wave = air_wave
 
         #Update self.const_pars to include the new values in fitpars
         #  I know, it's confusing that const_pars holds some non-constant parameters...
